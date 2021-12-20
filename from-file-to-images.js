11a3d0955d2f37efaaaca83aef48f501977f9b32
Berk <berkpeker94@gmail.com>
2021-12-20 11:14:23 +0300

    2ndCommit

--- from-file-to-images.js
+++ from-file-to-images.js
@@ -1,25 +1,28 @@
+//here we convert pdf to png images
+
 const { fromPath } = require("pdf2pic");
 const { mkdirsSync } = require("fs-extra");
 const rimraf = require("rimraf");
 
 module.exports = () => {
-  const pdfFile = "./files/specimen1.pdf";
+  const pdfFile = "./files/REGULAR SMALL.pdf";
   // const pdfFile = "./files/20 OZ.pdf";
+  // const pdfFile = "./files/REGULAR SMALL.png";
 
   const outputDirectory = "./output";
-  
+
   rimraf.sync(outputDirectory);
-  
+
   mkdirsSync(outputDirectory);
-  
+
   const baseOptions = {
     width: 2550,
     height: 3300,
     density: 330,
-    savePath: outputDirectory
+    savePath: outputDirectory,
   };
-  
+
   const convert = fromPath(pdfFile, baseOptions);
-  
+
   return convert.bulk(-1);
-}
+};
